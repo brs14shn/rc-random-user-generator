47f53615febea3a18baaa9c3965cc6c776e3117f
brs14shn <brsshn292929@gmail.com>
2022-07-04 12:42:21 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Card from  "./components/Card";
 
 
 
 const App = () => {
 
   const [user, setUser ] = useState()
+  // const [loading,setLoading] =useState(true)
   const url="https://randomuser.me/api/";
 
   const getUserRandom=async()=>{
     const {data}=await axios.get(url);
-    setUser(data)
+    setUser(data.results[0])
+    // setLoading(false)
   }
   // getUserRandom()
+
+  useEffect(()=>{
+   getUserRandom()
+  },[])
+
+  // if(loading){
+  //   return <h2>Loading...</h2>
+  // }
  
    console.log(user);
 
   return (
-    <div>
-      <h1></h1>
-      <img src="" alt="" />
+    <div className="container text-center">
+      <h1>UserList</h1>
+      <div className="card-container bgColor">
+    
+      {user && <Card user={user}/>}
+       
+      </div>
 
-      
+      <button className="btn btn-danger mt-4" onClick={()=>getUserRandom()}>
+        Random User
+      </button>
     </div>
+    
   )
 }
 
 export default App
+
+
+//*
---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+const Card = ({ user }) => {
+  console.log(user);
+  const {
+    picture: { medium },
+  } = user;
+
+  return (
+    <div>
+      <h1>Card oluştur</h1>
+      <img className="rounded-circle w-50" src={medium} alt="" />;
+    </div>
+  );
+};
+
+export default Card;
