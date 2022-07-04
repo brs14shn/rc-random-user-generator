5834191764c722be81a92cf7dfb0da97f8d9ef0c
brs14shn <brsshn292929@gmail.com>
2022-07-04 10:01:57 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-function App() {
+import axios from "axios";
+import { useState } from "react";
+
+
+
+const App = () => {
+
+  const [user, setUser ] = useState()
+  const url="https://randomuser.me/api/";
+
+  const getUserRandom=async()=>{
+    const {data}=await axios.get(url);
+    setUser(data)
+  }
+  // getUserRandom()
+ 
+   console.log(user);
+
   return (
     <div>
+      <h1></h1>
+      <img src="" alt="" />
+
       
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
